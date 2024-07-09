55540dade00e38ae7be52c932018522c3889f4b9
Eetu Sutinen <eetu.sutinen@gmail.com>
2024-07-09 18:57:52 +0300

    Use common eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,8 @@
-import eslintNodeConfig from "eslint-config-node";
-import reactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
-import reactRecommended from "eslint-plugin-react/configs/recommended.js";
-import reactRefresh from "eslint-plugin-react-refresh";
+import eslintReactConfig from "eslint-config-react";
 
 export default [
-  ...eslintNodeConfig,
+  ...eslintReactConfig,
   {
     files: ["**/*.ts", "**/*.tsx"],
-    ...reactRecommended,
-    ...reactJsxRuntime,
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    plugins: {
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-    },
   },
 ];
